6e2a562c6762eb83b88c1d2846ad7d452e8cddde
gustavo <gustavo.cesariosilva15@gmail.com>
2024-02-25 09:40:18 -0300

    Fidelius img

--- src/js/projetosWebData.js
+++
@@ -1,7 +0,0 @@
-const projetosWebData = [
-    { src: 'assets/Fidelius.png', alt: 'Software', tipo: 'Sistema de Semiologia Farmacêutica' },
-    { src: 'assets/gowdb.png', alt: 'API', tipo: 'API/GoW DB' },
-    { src: 'assets/games.png', alt: 'Website/Online Shop', tipo: 'Loja de Games' },
-  ];
-
-  export default projetosWebData
